55fb593a1a3b8676bf89f347bad7a04bb78fdcc0
Miguel Grimaldos <m.grimaldosmoreno@cranfield.com>
2014-08-09 05:50:55 +0100

    Transcoding on Device implemented. Transcoding on Server (Server has the file) implemented. Time measurements implemented

--- src/com/thesis/transcodingapp/MainActivity.java
+++ src/com/thesis/transcodingapp/MainActivity.java
@@ -1,21 +1,24 @@
 package com.thesis.transcodingapp;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 
+import com.thesis.offloadinglibrary.Util;
+
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	long beginning = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,78 +26,82 @@ public class MainActivity extends Activity {
 	}
 	
 	public void transcode(View view) throws Exception{
-		/*
-		final String path = Environment.getExternalStorageDirectory() + "DCIM/Camera/"; //android.os.Environment.DIRECTORY_DCIM + "/Camera/";
-		File source = new File(path + "source.mp4");
-		File target = new File(path + "target.avi");
-		*/
 		
-		Videokit vk = new Videokit();
-		/*
-		File images = new File(Environment.getExternalStorageDirectory(), "fun");
-		images.mkdirs();
-		for (int i=0; i<10; i++) {
-			String filename = String.format("snap%04d.jpg", i);
-			File dest = new File(images, filename);
-			Log.i("Test", "Adding image at " + dest.getAbsolutePath());
-			InputStream is = getInstrumentation().getContext().getAssets().open("image.jpg");
-			BufferedOutputStream o = null;
-			try {
-				byte[] buff = new byte[10000];
-				int read = -1;
-				o = new BufferedOutputStream(new FileOutputStream(dest), 10000);
-				while ((read = is.read(buff)) > -1) { 
-					o.write(buff, 0, read);
-				}
-			} finally {
-				is.close();
-				if (o != null) o.close();  
-				
-			}
+		beginning = System.nanoTime();
+		
+		RadioButton device = (RadioButton) findViewById(R.id.deviceRadioButton);
+		RadioButton server = (RadioButton) findViewById(R.id.serverRadioButton);
+		RadioButton auto = (RadioButton) findViewById(R.id.autoRadioButton);
+		TextView info = (TextView) findViewById(R.id.infoTextView);
+		
+		info.setText("Starting!");
+		
+		if (device.isChecked()){
+			info.setText("Transcoding on Device!");
+			new TranscodingTask().execute(Environment.getExternalStorageDirectory() + "/source.mp4", 
+					Environment.getExternalStorageDirectory() + "/target.avi");
+		} else if (server.isChecked()){
+			info.setText("Transcoding on Server!"); //Sending petition, bla bla bla
+			// The server already has the file
+			new Util().receiveFile(beginning);
+			scanFile(new File(Environment.getExternalStorageDirectory() + "/target.avi").getAbsolutePath());
+			//System.out.println("Time elapsed: " + (System.nanoTime() - beginning) + "ns");
+			//Sending the file
+			
+		} else if(auto.isChecked()) {
+			// Coming soon
 		}
-		*/
-		
-		//videokit.initialise();
-		
-		//File file = new File(images.getAbsolutePath(), "snap0000.jpg");
-		//assertTrue("File exist", file.exists());
-		
-		String input = new File(Environment.getExternalStorageDirectory(), "source.mp4").getAbsolutePath();//file.getAbsolutePath(); 
-		Log.i("Test", "Let's set input to " + input);
-//		videokit.setInputFile(input); 
-		//deleteFile(Environment.getExternalStorageDirectory().getAbsolutePath(), "dest.avi");
-		new File(Environment.getExternalStorageDirectory(), "dest.avi").delete();
-		String output = new File(Environment.getExternalStorageDirectory(), "dest.avi").getAbsolutePath();
-		Log.i("Test", "Let's set output to " + output);
-//		videokit.setOutputFile(output); 
-		
-		vk.run(new String[]{
-				"ffmpeg",
-				"-i",
-				input,
-				output
-		});
-		
-//		videokit.setSize(640,480); 
-//		videokit.setFrameRate(5);
-//		
-//		videokit.encode();
 	}
 	
-	private void deleteFile(String inputPath, String inputFile) {
-	    try {
-	        // delete the original file
-	        new File(inputPath, inputFile).delete();  
+	private class TranscodingTask extends AsyncTask<String, Integer, Integer>{
 
+		@Override
+		protected Integer doInBackground(String... params) {
+			
+			//
+			System.gc();
+			Videokit vk = new Videokit();
+			
+			String input = new File(params[0]).getAbsolutePath();//file.getAbsolutePath(); 
+			Log.i("Test", "Let's set input to " + input);
+			String output = new File(params[1]).getAbsolutePath();
+			Log.i("Test", "Let's set output to " + output);
+			
+			vk.run(new String[]{
+					"ffmpeg",
+					"-y",
+					"-i",
+					input,
+					output
+			});
+			//
+			
+			scanFile(new File(params[1]).getAbsolutePath());
+			
+			vk = null;
+			System.gc();
+			
+			return 1;//resul.toString();
+		}
+		
+		protected void onPostExecute(Integer res) {
+			TextView info = (TextView) findViewById(R.id.infoTextView);
+			info.setText("Complete!");
 
-	    }
-	    /*
-	   catch (FileNotFoundException fnfe1) {
-	        Log.e("tag", fnfe1.getMessage());
-	    }
-	    */
-	    catch (Exception e) {
-	        Log.e("tag", e.getMessage());
-	    }
+			System.out.println("Time elapsed: " + (System.nanoTime() - beginning) + "ns");
+		}
+		
 	}
+	
+	private void scanFile(String path) {
+
+        MediaScannerConnection.scanFile(MainActivity.this,
+                new String[] { path }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("TAG", "Finished scanning " + path);
+                    }
+                });
+    }
 }
